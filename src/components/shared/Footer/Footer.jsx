185b36a519f5f27e01591d8175991075cba73bb4
sohan-r30 <sohanakondo139@gmail.com>
2023-06-07 07:47:28 +0600

    sample footer added

---
+++ src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="fixed bottom-0 left-0 right-0">
+            <p className="text-center bg-primaryColor py-3 uppercase font-semibold tracking-wider">Copyright 2023 by summer Camp</p>
+        </div>
+    );
+};
+
+export default Footer;
