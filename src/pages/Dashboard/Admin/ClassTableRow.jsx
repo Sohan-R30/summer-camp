023e9a77331f0f30b3d53528d511e2eeb7f0e038
sohan-r30 <sohanakondo139@gmail.com>
2023-06-09 03:30:08 +0600

    added aproved functionality

--- src/pages/Dashboard/Admin/ClassTableRow.jsx
+++ src/pages/Dashboard/Admin/ClassTableRow.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
-const ClassTableRow = ({SingleClass,index}) => {
-    const {availableSeats,className,classPhoto,instructorEmail,instructorName,price,status} = SingleClass.storedClass    || {}
+const ClassTableRow = ({SingleClass,index,refetch}) => {
+    const {availableSeats,className,classPhoto,instructorEmail,instructorName,price,status} = SingleClass.storedClass || {}
+
+    const [disabledApproved, setdisabledApproved] = useState(false);
+    const [disabledDeny, setdisabledDeny] = useState(false);
+    const [disabledFeedback, setdisabledFeedback] = useState(false);
+    const [axiosSecure] = useAxiosSecure();
+
+    const handleApproved = (id) => {
+        console.log(id);
+
+        axiosSecure.patch(`/classes/admin/approved/${id}`)
+            .then((data) => {
+                console.log(data?.data)
+                if(data?.data?.modifiedCount){
+                    refetch()
+                    setdisabledApproved(true)
+                    Swal.fire({
+                        position: 'top-center',
+                        icon: 'success',
+                        title: 'Class Approved',
+                        showConfirmButton: false,
+                        timer: 500
+                      })
+                }
+            })
+            .catch(error => console.log(error))
+    }
+    
+
     return (
         <tr className="text-center divide-x divide-slate-300 divide-y divide-y-reverse divide-x-reverse shadow-sm">
             <td>{index + 1}</td>
@@ -20,16 +51,21 @@ const ClassTableRow = ({SingleClass,index}) => {
             <td>{status}</td>
             <th>
                 <button
-                 className={`${"bg-[#83e0f5] font-bold   hover:bg-[#7f9a9f] hover:text-white "}  py-2 px-3 rounded-lg` }
+                 onClick={() => handleApproved(SingleClass._id)} 
+                 disabled={disabledApproved || status === "approved" || status === "deny"}
+                 className={`${(status === "approved" || status === "deny") || disabledApproved ? "bg-gray-400 " : "bg-[#83e0f5] font-bold   hover:bg-[#7f9a9f] hover:text-white "}  py-2 px-3 rounded-lg` }
                  >Aprroved</button>
             </th>
             <th>
                 <button  
-                className={`bg-[#83e0f5] font-bold  hover:bg-[#7f9a9f] hover:text-white"}  py-2 px-3 rounded-lg`}
+                 onClick={() => handleDeny(SingleClass._id)} 
+                 disabled={disabledDeny || status === "approved" || status === "deny"}
+                className={`${(status === "approved" || status === "deny") || disabledApproved ? "bg-gray-400 " : "bg-[#83e0f5] font-bold  hover:bg-[#7f9a9f] hover:text-white"}  py-2 px-3 rounded-lg`}
                 >Deny</button>
             </th>
             <th>
-                <button  
+                <button   onClick={() => handleSendFeedBack(SingleClass._id)} 
+                // disabled={disabledFeedback || status === "approved" || status === "deny"}
                 className={`bg-[#83e0f5] font-bold  hover:bg-[#7f9a9f] hover:text-white"}  py-2 px-3 rounded-lg`}
                 >Send Feedback</button>
             </th>
